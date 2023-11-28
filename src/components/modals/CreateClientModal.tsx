4167b9f3eba03c7a61088ee937037a2a03707707
vague2k <ilovedrawing056@gmail.com>
2023-11-28 10:25:28 -0500

    TBD WIP: Use modals for confirmation of action

--- src/components/modals/CreateClientModal.tsx
+++ src/components/modals/CreateClientModal.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState, type ChangeEvent, type SyntheticEvent } from "react";
 import toast from "react-hot-toast";
-import { BiX } from "react-icons/bi";
+import { BiCheck, BiX } from "react-icons/bi";
 import Box from "../Box";
 import Button from "../Button";
 import Input from "../Input";
@@ -20,6 +20,7 @@ export default function CreateClientModal({
     name: "",
     email: "",
   });
+  const [wasDataCreated, setWasDataCreated] = useState(false);
 
   function onChange(event: ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
@@ -34,6 +35,7 @@ export default function CreateClientModal({
         headers: { "Content-Type": "application/json" },
       });
 
+      setWasDataCreated(true);
       refreshIfDataChange();
 
       toast.success(response.data.message);
@@ -50,58 +52,86 @@ export default function CreateClientModal({
 
   return (
     <div className="flex items-center justify-center fixed inset-0 bg-neutral-400 bg-opacity-30 backdrop-blur-sm">
-      <Box className="relative h-fit w-fit max-w-lg p-8">
-        <form onSubmit={(event) => handleSubmit(event)}>
-          <div className="relative flex flex-col justify-center gap-y-3 items-center">
-            <Button
-              onClick={onClose}
-              type="button"
-              className="absolute right-3 top-3 bg-transparent hover:bg-neutral-200 duration-300 text-neutral-800"
-            >
-              <BiX size={20} />
-            </Button>
-            <div className="flex justify-center items-center w-11 h-11 rounded-full bg-blue-200">
-              <Icon size={25} className="text-blue-500" />
+      {!wasDataCreated && (
+        <Box className="relative h-fit w-fit max-w-lg p-8">
+          <form onSubmit={(event) => handleSubmit(event)}>
+            <div className="relative flex flex-col justify-center gap-y-3 items-center">
+              <Button
+                onClick={onClose}
+                type="button"
+                className="absolute right-3 top-3 bg-transparent hover:bg-neutral-200 duration-300 text-neutral-800"
+              >
+                <BiX size={20} />
+              </Button>
+              <div className="flex justify-center items-center w-11 h-11 rounded-full bg-blue-200">
+                <Icon size={25} className="text-blue-500" />
+              </div>
+              <h1 className="font-semibold text-lg text-neutral-800">
+                {header}
+              </h1>
+
+              <Input
+                onChange={onChange}
+                name="name"
+                value={clientFormData.name}
+                placeholder="Name"
+              />
+              <Input
+                onChange={onChange}
+                name="email"
+                value={clientFormData.email}
+                placeholder="Email"
+              />
+
+              <p className="pb-3 font-regular text-sm text-center text-neutral-500">
+                The new client will have 0 orders by default. Don't worry, this
+                can be updated later!
+              </p>
             </div>
-            <h1 className="font-semibold text-lg text-neutral-800">{header}</h1>
 
-            <Input
-              onChange={onChange}
-              name="name"
-              value={clientFormData.name}
-              placeholder="Name"
-            />
-            <Input
-              onChange={onChange}
-              name="email"
-              value={clientFormData.email}
-              placeholder="Email"
-            />
+            <div className="flex justify-center items-center gap-x-3 pt-3">
+              <Button
+                onClick={onClose}
+                type="button"
+                className="bg-neutral-200 hover:bg-neutral-300 duration-300 text-neutral-800"
+              >
+                Cancel
+              </Button>
 
-            <p className="pb-3 font-regular text-sm text-center text-neutral-500">
-              The new client will have 0 orders by default. Don't worry, this
-              can be updated later!
+              <Button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-400 duration-300 text-neutral-50"
+              >
+                Create Client
+              </Button>
+            </div>
+          </form>
+        </Box>
+      )}
+
+      {wasDataCreated && (
+        <Box className="h-fit w-fit max-w-lg p-8">
+          <div className="flex flex-col justify-center gap-y-3 items-center px-8">
+            <div className="flex justify-center items-center w-11 h-11 rounded-full bg-green-500">
+              <BiCheck size={25} className="text-neutral-50" />
+            </div>
+            <h1 className="font-semibold text-lg text-neutral-800">
+              A new client was created!
+            </h1>
+            <p className="font-regular text-sm text-center text-neutral-500">
+              They will have 0 orders by default. They're details can be updated
+              later if you wish to do so!
             </p>
-          </div>
-
-          <div className="flex justify-center items-center gap-x-3 pt-3">
             <Button
               onClick={onClose}
               type="button"
               className="bg-neutral-200 hover:bg-neutral-300 duration-300 text-neutral-800"
             >
-              Cancel
-            </Button>
-
-            <Button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-400 duration-300 text-neutral-50"
-            >
-              Create Client
+              Dismiss
             </Button>
           </div>
-        </form>
-      </Box>
+        </Box>
+      )}
     </div>
   );
 }
--- src/components/modals/DeleteAllClientsModal.tsx
+++ src/components/modals/DeleteAllClientsModal.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { useState } from "react";
 import toast from "react-hot-toast";
-import { BiX } from "react-icons/bi";
+import { BiCheck, BiX } from "react-icons/bi";
 import Box from "../Box";
 import Button from "../Button";
 import type { ModalProps } from "./types";
@@ -15,14 +16,17 @@ export default function ConfirmDeleteModal({
 }: ModalProps & { refreshIfDataChange: () => void }) {
   if (!visible) return null;
 
+  const [wasAllDataDeleted, setWasAllDataDeleted] = useState(false);
   async function handleDeleteAllRecords() {
     try {
-      const response = await axios.delete("/api/clients/deleteall", {
+      await axios.delete("/api/clients/deleteall", {
         headers: { "Content-Type": "application/json" },
       });
 
+      // TODO: handle if delete all button is clicked when there is no data to delete
+
+      setWasAllDataDeleted(true);
       refreshIfDataChange();
-      toast.success(response.data.message);
     } catch (error: unknown) {
       if (axios.isAxiosError(error)) {
         toast.error(error.response?.data.message);
@@ -36,40 +40,61 @@ export default function ConfirmDeleteModal({
 
   return (
     <div className="flex items-center justify-center fixed inset-0 bg-neutral-400 bg-opacity-30 backdrop-blur-sm">
-      <Box className="relative h-fit w-fit max-w-lg p-8">
-        <div className="flex flex-col justify-center gap-y-3 items-center">
-          <Button
-            onClick={onClose}
-            type="button"
-            className="absolute right-3 top-3 bg-transparent hover:bg-neutral-200 duration-300 text-neutral-800"
-          >
-            <BiX size={20} />
-          </Button>
-          <div className="flex justify-center items-center w-11 h-11 rounded-full bg-red-200">
-            <Icon size={25} className="text-red-500" />
+      {!wasAllDataDeleted && (
+        <Box className="relative h-fit w-fit max-w-lg p-8">
+          <div className="flex flex-col justify-center gap-y-3 items-center">
+            <Button
+              onClick={onClose}
+              type="button"
+              className="absolute right-3 top-3 bg-transparent hover:bg-neutral-200 duration-300 text-neutral-800"
+            >
+              <BiX size={20} />
+            </Button>
+            <div className="flex justify-center items-center w-11 h-11 rounded-full bg-red-200">
+              <Icon size={25} className="text-red-500" />
+            </div>
+            <h1 className="font-semibold text-lg text-neutral-800">{header}</h1>
+            <p className="font-regular text-sm text-center text-neutral-500">
+              {description}
+            </p>
           </div>
-          <h1 className="font-semibold text-lg text-neutral-800">{header}</h1>
-          <p className="font-regular text-sm text-center text-neutral-500">
-            {description}
-          </p>
-        </div>
-        <div className="flex justify-center items-center gap-x-3 pt-3">
-          <Button
-            onClick={onClose}
-            type="button"
-            className="bg-neutral-200 hover:bg-neutral-100 duration-300 text-neutral-800"
-          >
-            No, cancel everything
-          </Button>
-          <Button
-            onClick={handleDeleteAllRecords}
-            type="button"
-            className="bg-red-600 hover:bg-red-500 duration-300 text-neutral-50"
-          >
-            Yes, Delete all records
-          </Button>
-        </div>
-      </Box>
+          <div className="flex justify-center items-center gap-x-3 pt-3">
+            <Button
+              onClick={onClose}
+              type="button"
+              className="bg-neutral-200 hover:bg-neutral-100 duration-300 text-neutral-800"
+            >
+              No, cancel everything
+            </Button>
+            <Button
+              onClick={handleDeleteAllRecords}
+              type="button"
+              className="bg-red-600 hover:bg-red-500 duration-300 text-neutral-50"
+            >
+              Yes, Delete all records
+            </Button>
+          </div>
+        </Box>
+      )}
+      {wasAllDataDeleted && (
+        <Box className="h-fit w-fit max-w-lg p-8">
+          <div className="flex flex-col justify-center gap-y-3 items-center px-8">
+            <div className="flex justify-center items-center w-11 h-11 rounded-full bg-green-500">
+              <BiCheck size={25} className="text-neutral-50" />
+            </div>
+            <h1 className="font-semibold text-lg text-neutral-800">
+              All clients have been deleted.
+            </h1>
+            <Button
+              onClick={onClose}
+              type="button"
+              className="bg-neutral-200 hover:bg-neutral-300 duration-300 text-neutral-800"
+            >
+              Dismiss
+            </Button>
+          </div>
+        </Box>
+      )}
     </div>
   );
 }
