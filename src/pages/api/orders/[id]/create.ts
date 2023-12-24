65332f205445dcbc9acbf13ffa3e7a5e5da75fa6
vague2k <121782036+vague2k@users.noreply.github.com>
2023-12-24 09:35:33 -0500

    Create order works as intended

--- src/pages/api/orders/[id]/create.ts
+++ src/pages/api/orders/[id]/create.ts
@@ -8,7 +8,7 @@ export const POST: APIRoute = async ({ params }) => {
 
     try {
 
-        if (id === undefined) {
+        if (!id) {
             return new Response(
                 JSON.stringify(
                     { message: "/orders/[id]/create is missing a url param. This should be a client's record id" }
@@ -25,12 +25,18 @@ export const POST: APIRoute = async ({ params }) => {
                 { status: 400 }
             );
         }
+
         // get query object filtered by if the order relationship is null and by the clients name and email
+        /*
+         * We filter by these parameters to make sure an order detail that already has a specific order 
+         * associated with it does not get summarized into our new order later on
+         */
+
         const filteredDetail = xata.db.order_details
             .filter({ $notExists: "orders" })
-            .filter({ clientName: client?.name, clientEmail: client?.email })
+            .filter({ clientName: client.name, clientEmail: client.email })
 
-        // aggregate data with Xata specific API operations on all order details 
+        // summarize data with Xata specific API operations on all order details 
         // this returns an array of 1 single object
         // [0]'th index will be used to select the object's properties later on
         const summarized = await filteredDetail.summarize({
@@ -40,27 +46,32 @@ export const POST: APIRoute = async ({ params }) => {
                 averageUnitPrice: { average: "unitPrice" }
             }
         })
+        const summaries = summarized.summaries[0]
 
         // construct order object based on data proccessed above
-        // TODO: How can we increment the amountOfOrders count whenever an order is placed?
         const newOrderObject: Orders = {
-            totalAmount: summarized.summaries[0].totalAmount,
-            totalUnits: summarized.summaries[0].totalUnits,
-            averageUnitPrice: summarized.summaries[0].averageUnitPrice,
+            totalAmount: summaries.totalAmount,
+            totalUnits: summaries.totalUnits,
+            averageUnitPrice: summaries.averageUnitPrice,
             orderDate: new Date(),
             complete: false,
-            clients: {
-                id: id,
-                name: client.name,
-                email: client.email,
-                amountOfOrders: client.amountOfOrders
-            }
+            // FIX: Works as intended but this type error is getting annoying to look at
+            clients: { id: id }
         }
 
+        // Finally, create a new orders
+        // Update the respective client's <amountOfOrders> count by incrementing by 1
+        // select all id's from filtered details, and update the orders field associated on that id
         const newOrder = await xata.db.orders.create(newOrderObject)
+        await xata.db.clients.update(id, { amountOfOrders: { $increment: 1 } })
+
+        const test = await filteredDetail.select(["id"]).getMany()
+        for (const orderDetail of test) {
+            await xata.db.order_details.update(orderDetail.id, { orders: newOrder.id })
+        }
 
         return new Response(
-            JSON.stringify({ newOrder, message: "check log" }),
+            JSON.stringify({ newOrder, message: "New order created!" }),
             {
                 status: 201,
                 headers: { "Content-Type": "application/json" }
@@ -75,4 +86,3 @@ export const POST: APIRoute = async ({ params }) => {
         );
     }
 };
-
